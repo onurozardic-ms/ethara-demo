75e46e7dbe66e857a0738348e95d17edabd09ecf
onurozardic-ms <124796289+onurozardic-ms@users.noreply.github.com>
2023-05-29 10:32:03 +0300

    updates for HelloWorld class

---
+++ CrossPlatformConsoleApplication/HelloWorld.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CrossPlatformConsoleApplication
+{
+    public class HelloWorld
+    {
+        private void HelloFromPrivate()
+        {
+            Console.WriteLine("Hello World from private");
+        }
+
+        public void HelloFromPublic()
+        {
+            Console.WriteLine("Hello World from public");
+        }
+
+        public int sum(int a, int b)
+        {
+            return a + b;
+        }
+
+        public void callHelloInPrivate()
+        {
+            this.HelloFromPrivate();
+        }
+    }
+}
--- CrossPlatformConsoleApplication/Program.cs
+++ CrossPlatformConsoleApplication/Program.cs
@@ -1,2 +1,9 @@
 ﻿// See https://aka.ms/new-console-template for more information
+using CrossPlatformConsoleApplication;
+using System.Runtime.CompilerServices;
+
 Console.WriteLine("Hello, World!");
+HelloWorld helloWorld = new HelloWorld();
+helloWorld.callHelloInPrivate();
+helloWorld.HelloFromPublic();
+Console.WriteLine( helloWorld.sum(3,5));
